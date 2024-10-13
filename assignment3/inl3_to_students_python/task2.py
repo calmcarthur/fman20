c25479c34b69555f27eaa620bae86d40b56597e0
cal <mcarthur.cal@gmail.com>
2024-10-13 15:12:35 +0200

    task2

--- assignment3/inl3_to_students_python/task2.py
+++ assignment3/inl3_to_students_python/task2.py
@@ -11,8 +11,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn import svm
-from sklearn import neighbors 
-
+from sklearn import neighbors
 
 
 if __name__ == "__main__":
@@ -36,66 +35,65 @@ if __name__ == "__main__":
         nbr_train_examples = np.size(Y_train,0)
         nbr_test_examples = np.size(Y_test,0)
         
-        
-        
+        # TREE MODEL
         tree_model = tree.DecisionTreeClassifier()
         tree_model = tree_model.fit(X_train,Y_train)
         
-        
-        # FILL IN COMMANDS FOR A LINEAR SVM USING THE SVC CLASSIFIER
-        # svm_model = ...
-        
-        
-        # FILL IN COMMANDS FOR A NEAREST NEIGHBOR CLASSIFIER
-        # USING THE KNeighborsClassifier
-        # nn_model = ...
-        
-        
+        # LINEAR SVM
+        svm_model = svm.SVC(kernel='linear')
+        svm_model = svm_model.fit(X_train, Y_train)
+  
+        # KNEIGHBOURS
+        nn_model = neighbors.KNeighborsClassifier()
+        nn_model = nn_model.fit(X_train, Y_train)
 
-    
+        # MY IMPLEMENTATION
+        
         # Next, let's use our trained model to classify the examples in the 
         # test data. 
-        predictions_test_tree  = tree_model.predict(X_test)
-        
+        predictions_test_tree = tree_model.predict(X_test)
+
         # FILL IN SIMILAR FOR THE OTHER MODELS
-        # predictions_test_svm  = ...
-        # predictions_test_nn  = ...
-        
-        
+        predictions_test_svm = svm_model.predict(X_test)
+        predictions_test_nn = nn_model.predict(X_test)
        
         # We can now proceed to computing the respective error rates.
         pred_test_diff_tree = predictions_test_tree - Y_test
         
         # FILL IN SIMILAR FOR THE OTHER MODELS
-        # pred_test_diff_svm = ...
-        # pred_test_diff_nn = ...
+        pred_test_diff_svm = predictions_test_svm - Y_test
+        pred_test_diff_nn = predictions_test_nn - Y_test
         
         err_rate_test_tree =  np.count_nonzero(pred_test_diff_tree) / nbr_test_examples 
         
         # FILL IN SIMILAR FOR THE OTHER MODELS
-        # err_rate_test_svm =  
-        # err_rate_test_nn =  
-        
+        err_rate_test_svm = np.count_nonzero(pred_test_diff_svm) / nbr_test_examples
+        err_rate_test_nn = np.count_nonzero(pred_test_diff_nn) / nbr_test_examples
         
         # Store them in the containers
         err_rates_test[i,0] = err_rate_test_tree
         
         # FILL IN SIMILAR FOR THE OTHER MODELS
-        # err_rates_test[i,1] = ...
-        # err_rates_test[i,2] = ...
+        err_rates_test[i, 1] = err_rate_test_svm
+        err_rates_test[i, 2] = err_rate_test_nn
             
         
-        # Next, let's do the same for the train data
-        
-        predictions_train_tree  = tree_model.predict(X_train)
-        # FILL IN REST SIMILAR TO THE TEST DATA ... 
-        
-        
-       
-        
-        
-        
-    
+        # TRAIN DATA
+        predictions_train_tree = tree_model.predict(X_train)
+        predictions_train_svm = svm_model.predict(X_train)
+        predictions_train_nn = nn_model.predict(X_train)
+
+        pred_train_diff_tree = predictions_train_tree - Y_train
+        pred_train_diff_svm = predictions_train_svm - Y_train
+        pred_train_diff_nn = predictions_train_nn - Y_train
+
+        err_rate_train_tree = np.count_nonzero(pred_train_diff_tree) / nbr_train_examples
+        err_rate_train_svm = np.count_nonzero(pred_train_diff_svm) / nbr_train_examples
+        err_rate_train_nn = np.count_nonzero(pred_train_diff_nn) / nbr_train_examples
+
+        err_rates_train[i, 0] = err_rate_train_tree
+        err_rates_train[i, 1] = err_rate_train_svm
+        err_rates_train[i, 2] = err_rate_train_nn
 
     print(np.mean(err_rates_test,0))
     print(np.mean(err_rates_train,0))
